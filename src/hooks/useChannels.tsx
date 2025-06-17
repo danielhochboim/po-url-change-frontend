7d8e041e4db171f7d3e44382d5fc8a3fd64ed6fa
danielhochboim <daniel.hochboim@gmail.com>
2025-06-17 17:14:54 +0300

    Remove all files

--- src/hooks/useChannels.tsx
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { api } from '@/lib/api';
-
-export const useChannels = () =>
-  useQuery({
-    queryKey: ['channels'],
-    queryFn: api.list,
-  });
